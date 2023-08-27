758b4ac5417d9aac0940cf57ae05f2490c2d3fc0
Zachatoo <Zachatoo@users.noreply.github.com>
2023-08-27 11:09:55 -0600

    Remove unused code

--- src/obsidian/vault-helpers.ts
+++ src/obsidian/vault-helpers.ts
@@ -1,16 +1,4 @@
-import { TFolder, type TAbstractFile, TFile, Vault } from "obsidian";
-
-export function getMarkdownFilesAndFolders(vault: Vault): TAbstractFile[] {
-	return vault.getAllLoadedFiles().filter((x) => {
-		if (x instanceof TFolder) {
-			return true;
-		}
-		if (x instanceof TFile) {
-			return x.extension === "md";
-		}
-		return false;
-	});
-}
+import { TFolder, Vault } from "obsidian";
 
 export function getFolders(vault: Vault): TFolder[] {
 	return vault
